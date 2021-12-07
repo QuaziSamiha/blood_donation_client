c012feb960893e89a31368e6feb0314f0c65d43a
QuaziSamiha <1703155@student.ruet.ac.bd>
2021-12-07 21:29:33 +0600

    tailwindcss installed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 function App() {
   return (
     <div>
+      <h1 className='bg-red-400'>this is app</h1>
     </div>
   );
 }
---
+++ src/components/Home/HeaderSlider/HeaderSlider.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HeaderSlider() {
+    return (
+        <div>
+            <h1>this is header slider</h1>
+        </div>
+    )
+}
